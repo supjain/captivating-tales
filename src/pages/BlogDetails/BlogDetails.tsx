2ebe671f50052b4611192d0e00a609e986f125de
Ojha <haojha@deloitte.com>
2023-06-01 13:22:15 +0530

    static blog details page

--- src/pages/BlogDetails/BlogDetails.tsx
+++ src/pages/BlogDetails/BlogDetails.tsx
@@ -3,7 +3,11 @@ const BlogDetails = () => {
     
     return (
     <div className="blog-details-page">
-     <p>BlogDetails</p> 
+    <h3>Blog Title</h3>
+    <hr />
+    <h5>Authored By: First Name</h5>
+    <h5>Published on: 12/13/2022</h5>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
     );
   }
