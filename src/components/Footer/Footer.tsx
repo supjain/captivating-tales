bf6eceab144e24433bf58f343d92a33c5136a735
Ojha <haojha@deloitte.com>
2023-06-01 16:36:30 +0530

    fixed text alignment and css in header and footer,added test test cases for BlogDetails page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,17 +3,24 @@ import './Footer.css';
 
 const  Footer = () => {
   return (<div className="footer-box">
+    <div className='footer-left'>
     <div>
     <Link className="about-link" to='/about'>
       About
     </Link>
     </div>
-   <div>
+    <div>
     <Link className="contact-link" to='/contacts'>
       Contacts
     </Link>
     </div>
+    </div>
+    <div className='footer-right'>
     <div className='copyright-claim'><p>© 2023 - Captivating Tales</p></div>
+    </div>
+    
+   
+    
   </div>
   );
 }
