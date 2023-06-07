37088a3ccc9bdb7e8550495e537b0720ed44e423
Ojha <haojha@deloitte.com>
2023-06-07 15:07:55 +0530

    api integration for comments on blogDetails page

--- src/pages/BlogDetails/BlogDetails.tsx
+++ src/pages/BlogDetails/BlogDetails.tsx
@@ -1,8 +1,18 @@
 import { useParams } from 'react-router-dom';
 import './BlogDetails.css'
 import BlogData from '../../utils/BlogData';
+import { useEffect, useState } from 'react';
+import fetchCommentsByPostID from '../../utils/apis/api';
+
 const BlogDetails = () => {
   const { id } = useParams();
+  const [comments,setComments]=useState([])
+  
+  useEffect(() => {
+    fetchCommentsByPostID(id)
+      .then(jsonData => setComments(jsonData.comments))
+      .catch(error => console.error('Error fetching data:', error));
+  }, [id]);
 
   const blogData=BlogData.filter((blog)=>(blog.id).toString()===id)
     return (
@@ -12,8 +22,21 @@ const BlogDetails = () => {
     <h5>Authored By: {blogData[0].author}</h5>
     <h5>Published on: {blogData[0].date}</h5>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <div className="comments">
+      <h4>Comments</h4>
+      {comments.length>0?comments.map((comment:any)=>{
+        return ( <div className="comment">
+        <h6>{comment?.user?.username}</h6>
+        <p>{comment?.body}</p>
+      </div>
+      )
+      }):
+      <p>Loading comments...</p>}
+    </div>
     </div>
     );
   }
   
   export default BlogDetails;
+
+
--- src/utils/apis/api.js
+++ src/utils/apis/api.js
@@ -1,2 +1,15 @@
 //To have all the api calls segregated
 //Please add your api calls here
+
+const fetchCommentsByPostID = async (postId) => {
+    try {
+      const response = await fetch(`https://dummyjson.com/comments/post/${postId}`);
+      const jsonData = await response.json();
+      return jsonData;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
+  };
+  
+  export default fetchCommentsByPostID;
